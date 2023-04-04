c1a62870a94d5acc71c21f8492081602b7262d4f
Fuzen2478 <fuzen2478@gmail.com>
2023-04-04 22:30:46 +0900

    add TODO each component I can finish maybe in this week...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 import BottomTab from "./components/BottomTab/BottomTab";
 
 function App() {
-  return (
-    <div className="App">
-      <BottomTab />
-    </div>
-  )
+    return (
+        <div className="App">
+            <BottomTab />
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/BusMap/BusMap.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BusMap: React.FC = () => {
+
+
+};
+
+
+
+
+{/*
+    TODO: 맵박스, 버스 아이콘을 지정된 값(경도, 위도)에 따라 표시, 버스 공지 생성
+*/
+}
